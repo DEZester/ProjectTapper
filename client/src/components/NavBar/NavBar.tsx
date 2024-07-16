66e410127442797b6de8d4ea110130299f0b4fd1
DEZester <aromudez@gmail.com>
2024-07-17 00:14:07 +0300

    Add NavBarButton component and style NavBar
    A new component, NavBarButton, is introduced in the UI/Buttons directory. In addition, styling is added to the NavBar by embedding NavBarButton, and configuring styles using styled-components and theme settings. This change improves the look and feel of the application's navigation feature.

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import {theme_default} from "@/styles/theme.ts";
+import NavBarButton from "@/components/UI/Buttons/NavBarButton.tsx";
+
+const createStyledNavBar = (theme: typeof theme_default) => styled.div`
+  position: relative;
+  display: flex;
+  height: 100vh;
+  width: 400px;
+  background-color: ${theme.lightPrimaryColor};
+`
+
+const StyledNavBar = createStyledNavBar(theme_default);
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  return <StyledNavBar>
+    NavBar
+    {
+      //links
+    }
+    <NavBarButton/>
+  </StyledNavBar>
 }
 
 export default NavBar
---
+++ client/src/components/UI/Buttons/NavBarButton.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Styles = styled.button`
+  position: absolute;
+  top: 50%;
+  right: -40px;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+`
+
+const NavBarButton = () => <Styles>Off</Styles>
+
+export default NavBarButton
