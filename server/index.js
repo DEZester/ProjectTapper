97a58d8ad438e0b5912a7cf3c1a2c62acb4b09f3
DEZester <aromudez@gmail.com>
2024-09-11 20:53:46 +0300

    Add MongoDB integration with mongoose
    Integrate MongoDB using the mongoose library to manage database connections. Update server to handle JSON requests and modify package dependencies accordingly. Enhance error handling for MongoDB connection issues.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import {textWithColor} from "./helpers/helpers.js";
+import mongoose from "mongoose";
 
 dotenv.config()
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(textWithColor(`Listening on ${PORT}`)))
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => app.listen(8080, () => console.log(textWithColor(`Listening on ${PORT}`))))
+  .catch((err) => console.error(textWithColor(`Error connecting to MongoDB: ${err}`)))
+
