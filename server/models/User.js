772babac837bb504c5c0c4167a54b512dd2ee60c
DEZester <aromudez@gmail.com>
2024-09-11 23:09:21 +0300

    Add User model for MongoDB
    Introduce a User model using mongoose to manage user-related data. The schema includes fields for name, age, email, password, and various user-specific attributes. The model also supports relationships with posts and maintains metadata with timestamps.

---
+++ server/models/User.js
@@ -0,0 +1,52 @@
+import mongoose, {Schema} from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false
+    },
+    game_user_id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    posts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      default: []
+    }],
+    friends: [Number],
+    achievments: {},
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    },
+    blocked_users: [Number],
+    last_login_at: Date,
+    created_at: Date,
+    updated_at: Date,
+  }, {timestamps: true}
+)
+
+const User = mongoose.model("User", UserSchema);
+export default User;
